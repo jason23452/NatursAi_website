2271ab3419e1ed513c0ca11ec4c0aa17760e77eb
jason23452 <andt30610@gmail.com>
2024-11-09 02:54:04 +0800

    0.0.9

--- src/my_header_menu/about.js
+++ src/my_header_menu/about.js
@@ -14,14 +14,14 @@ const CustomSelect = ({ options, name, value, onChange }) => {
     const handleBlur = () => setIsOpen(false);
 
     return (
-        <div className="relative w-[44.010vw] h-[9.935vh] flex items-center">
+        <div className="relative w-[44.010vw] h-[4.714vw] flex items-center">
             <select
                 name={name}
                 value={value}
                 onChange={onChange}
                 onFocus={handleClick}
                 onBlur={handleBlur}
-                className="w-full h-full appearance-none text-[#538AA1] text-[1.667vw] bg-white rounded-[0.521vw] shadow-lg pl-[0.971vw] font-normal font-Noto_Sans_ch tracking-[0.167vw] leading-[5.269vh]"
+                className="w-full h-full appearance-none text-[#538AA1] text-[1.667vw] bg-white rounded-[0.521vw] shadow-lg pl-[0.971vw] font-normal font-Noto_Sans_ch tracking-[0.167vw] leading-[2.500vw]"
             >
                 <option value="">請選擇</option>
                 {options.map((option, index) => (
@@ -30,7 +30,7 @@ const CustomSelect = ({ options, name, value, onChange }) => {
             </select>
 
             <img
-                className="absolute right-[2.013vw] w-[1.927vw] h-[2.086vh] pointer-events-none"
+                className="absolute right-[2.013vw] w-[1.927vw] h-[0.990vw] pointer-events-none"
                 src={isOpen ? openArrow : closedArrow}
                 alt="arrow"
             />
@@ -67,18 +67,18 @@ function About() {
 
     return (
         <div className="relative w-full h-full">
-            <div className='absolute w-[100%] max-w-[99.115vw] -top-[8.343vh]'>
+            <div className='absolute w-[100%]  -top-[3.958vw]'>
                 <img src={About_us_bg} className="w-full object-cover" alt="Login Background" />
                 <div className='absolute inset-0'>
-                    <div className='ml-[38.229vw] mt-[22.613vh] relative w-[17.760vw] h-[13.941vh]'>
-                        <img className='w-[17.760vw] h-[11.526vh]' src={title_img} />
-                        <span className='absolute left-[8.854vw] top-[8.782vh] w-[15.469vw] h-[4.830vh] flex justify-start items-center text-[#538AA2] text-[1.042vw] font-Noto_Sans_ch font-bold leading-[2.649vh] tracking-[0.156vw]'>
+                    <div className='ml-[38.229vw] mt-[10.729vw] relative w-[17.760vw] h-[6.615vw]'>
+                        <img className='w-[17.760vw] h-[5.469vw]' src={title_img} />
+                        <span className='absolute left-[8.854vw] top-[4.167vw] w-[15.469vw] h-[2.292vw] flex justify-start items-center text-[#538AA2] text-[1.042vw] font-Noto_Sans_ch font-bold leading-[1.257vw] tracking-[0.156vw]'>
                             劃時代的AI健康管理
                         </span>
                     </div>
-                    <form onSubmit={handleSubmit} className='ml-[26.3vw] mt-[2.1vh] w-[44.062vw] h-[143.249vh] flex flex-col'>
-                        <div className='w-[44.010vw] h-[17.311vh] flex flex-col justify-between'>
-                            <span className='w-[18.654vw] h-[6.146vh] text-[1.875vw] font-Noto_Sans_ch font-bold leading-[5.928vh]'>信息內容</span>
+                    <form onSubmit={handleSubmit} className='ml-[26.3vw] mt-[0.996vw] w-[44.062vw] h-[67.968vw] flex flex-col'>
+                        <div className='w-[44.010vw] h-[8.196vw] flex flex-col justify-between'>
+                            <span className='w-[18.654vw] h-[2.916vw] text-[1.875vw] font-Noto_Sans_ch font-bold leading-[2.812vw]'>信息內容</span>
                             <CustomSelect
                                 name="content"
                                 options={['技術支援', '產品意見', '刪除帳號', '合作機會', '其他']}
@@ -86,8 +86,8 @@ function About() {
                                 onChange={handleChange}
                             />
                         </div>
-                        <div className='mt-[7.266vh] w-[44.010vw] h-[17.311vh] flex flex-col justify-between'>
-                            <span className='w-[18.654vw] h-[6.146vh] text-[1.875vw] font-Noto_Sans_ch font-bold leading-[5.928vh]'>您的身份</span>
+                        <div className='mt-[3.448vw] w-[44.010vw] h-[8.196vw] flex flex-col justify-between'>
+                            <span className='w-[18.654vw] h-[2.916vw] text-[1.875vw] font-Noto_Sans_ch font-bold leading-[2.812vw]'>您的身份</span>
                             <CustomSelect
                                 name="identity"
                                 options={['夠益營養APP使用者', '照護人員', '醫護人員', '合作關係人', '其他']}
@@ -95,45 +95,45 @@ function About() {
                                 onChange={handleChange}
                             />
                         </div>
-                        <div className='w-[43.438vw] h-[18.551vh] mt-[8.9vh] flex justify-between'>
-                            <div className='w-[20.573vw] h-[18.551vh] flex flex-col'>
+                        <div className='w-[43.438vw] h-[8.801vw] mt-[4.223vw] flex justify-between'>
+                            <div className='w-[20.573vw] h-[8.801vw] flex flex-col'>
                                 <span className='text-[1.875vw] font-bold'>姓名<span className='text-[#AE2D2D]'>*</span></span>
                                 <input
                                     name="name"
                                     value={formData.name}
                                     onChange={handleChange}
-                                    className='w-[20.573vw] h-[10.648vh] pl-[0.990vw] bg-white rounded-[0.521vw] shadow-lg placeholder:text-[#CACACA] text-[1.667vw]'
+                                    className='w-[20.573vw] h-[5.052vw] pl-[0.990vw] bg-white rounded-[0.521vw] shadow-lg placeholder:text-[#CACACA] text-[1.667vw]'
                                     placeholder='你的姓名'
                                     required
                                 />
                             </div>
-                            <div className='w-[20.573vw] h-[18.551vh] flex flex-col'>
+                            <div className='w-[20.573vw] h-[8.801vw] flex flex-col'>
                                 <span className='text-[1.875vw] font-bold'>信箱<span className='text-[#AE2D2D]'>*</span></span>
                                 <input
                                     name="email"
                                     value={formData.email}
                                     onChange={handleChange}
                                     type="email"
-                                    className='w-[20.573vw] h-[10.648vh] pl-[0.990vw] bg-white rounded-[0.521vw] shadow-lg placeholder:text-[#CACACA] text-[1.667vw]'
+                                    className='w-[20.573vw] h-[5.052vw] pl-[0.990vw] bg-white rounded-[0.521vw] shadow-lg placeholder:text-[#CACACA] text-[1.667vw]'
                                     placeholder='你的信箱'
                                     required
                                 />
                             </div>
                         </div>
-                        <div className='mt-[7.684vh] w-[44.010vw] h-[48.628vh] flex flex-col justify-between'>
+                        <div className='mt-[3.646vw] w-[44.010vw] h-[23.044vw] flex flex-col justify-between'>
                             <span className='text-[1.875vw] font-bold'>問題意見<span className='text-[#AE2D2D]'>*</span></span>
                             <textarea
                                 name="message"
                                 value={formData.message}
                                 onChange={handleChange}
-                                className='w-[44.010vw] h-[40.724vh] bg-white shadow-lg pl-[1.042vw] pt-[1.976vh] rounded-[0.521vw] placeholder:text-[#CACACA] text-[1.667vw] resize-none'
+                                className='w-[44.010vw] h-[19.322vw] bg-white shadow-lg pl-[1.042vw] pt-[0.938vw] rounded-[0.521vw] placeholder:text-[#CACACA] text-[1.667vw] resize-none'
                                 placeholder='請輸入問題意見'
                                 required
                             ></textarea>
                         </div>
-                        <div className='w-[44.062vw] h-[11.526vh] flex justify-between mt-[6.257vh]'>
-                            <img src={no_robot} className='w-[20.052vw] h-[11.526vh]' />
-                            <button type="submit" className='mt-[4.281vh] w-[11.667vw] h-[6.147vh] bg-gradient-to-r from-[#399983] to-[#538aa2] text-white text-[1.250vw] font-medium hover:from-[#3F3F46] hover:to-[#3F3F46]'>
+                        <div className='w-[44.062vw] h-[5.469vw] flex justify-between mt-[2.969vw]'>
+                            <img src={no_robot} className='w-[20.052vw] h-[5.469vw]' />
+                            <button type="submit" className='mt-[2.031vw] w-[11.667vw] h-[2.916vw] bg-gradient-to-r from-[#399983] to-[#538aa2] text-white text-[1.250vw] font-medium hover:from-[#3F3F46] hover:to-[#3F3F46]'>
                                 提交
                             </button>
                         </div>
--- src/my_header_menu/home.js
+++ src/my_header_menu/home.js
@@ -40,98 +40,106 @@ function Home() {
 
     return (
         <div className="relative w-full h-full">
-            <div className='absolute w-[100%] max-w-[99.115vw] -top-[8.343vh]'>
+            <div className='absolute w-full  -top-[3.958vw]'>
                 <img src={home_bg} className="w-full object-cover" alt="Login Background" />
                 {/* <img src={test} className=' absolute top-0 opacity-60' /> */}
                 <div className='  absolute inset-0 '>
-                    <div className='w-[88.594vw] h-[74.314vh] ml-[9.271vw] mt-[27.881vh] flex justify-between'>
-                        <div className='w-[33.125vw] h-[38.529vh] flex flex-col'>
-                            <span className="w-[21.354vw] h-[12.953vh] text-[#538aa2] text-[1.667vw] font-normal font-Baloo_Thambi leading-[6.2vh] tracking-[0.250vw]">Naturs
-                                <span className='text-[#399983] text-[1.667vw] font-normal font-Baloo_Thambi leading-[6vh] tracking-[0.250vw]'>ai <br />
-                                    <span className="text-[#538aa2] text-[1.667vw] font-bold font-Noto_Sans_ch leading-[6.vh] tracking-[0.250vw]">劃時代的AI健康管理</span>
+                    <div className='w-[88.594vw] h-[35.260vw] ml-[9.271vw] mt-[13.229vw] flex justify-between'>
+
+                        <div className='w-[33.125vw] h-[18.281vw] flex flex-col'>
+                            <span className="w-[21.354vw] h-[6.146vw] text-[#538aa2] text-[1.667vw] font-normal font-Baloo_Thambi leading-[3.000vw] tracking-[0.250vw]">Naturs
+                                <span className='text-[#399983] text-[1.667vw] font-normal font-Baloo_Thambi leading-[3.000vw] tracking-[0.250vw]'>ai <br />    
+                                    <span className="text-[#538aa2] text-[1.667vw] font-bold font-Noto_Sans_ch leading-[3.000vw] tracking-[0.250vw]">劃時代的AI健康管理</span>
                                 </span>
                             </span>
-                            <span className='w-[33.125vw] h-[17.563vh] mt-[1.098vh] text-[#3F3F46] text-[1.042vw] font-normal font-Noto_Sans_ch leading-[2.649vh] tracking-[0.156vw]'><br />我們著重於打造真實虛實體驗，將現實生活中的健康管理透過與各健康專案、遊戲、活動結合形成虛實整合的強大工作，讓人從小事情開始關注健康議題，比較枯燥乏味的健康管理，立即加入
-                                <span className="text-[#538aa2] text-[1.042vw] font-normal font-Baloo_Thambi leading-[2.649vh] tracking-[0.156vw]">Naturs
-                                    <span className="text-[#399983] text-[1.042vw] font-normal font-Baloo_Thambi leading-[2.649vh] tracking-[0.156vw]">ai
-                                        <span className="text-[#3F3F46] text-[1.042vw] font-normal font-Noto_Sans_ch leading-[2.649vh] tracking-[0.156vw]">所有產品，體驗真實的生活管理系統。<br /></span>
+                            
+                            <span className='w-[33.125vw] h-[8.333vw] mt-[0.521vw] text-[#3F3F46] text-[1.042vw] font-normal font-Noto_Sans_ch leading-[1.257vw] tracking-[0.156vw]'><br />我們著重於打造真實虛實體驗，將現實生活中的健康管理透過與各健康專案、遊戲、活動結合形成虛實整合的強大工作，讓人從小事情開始關注健康議題，比較枯燥乏味的健康管理，立即加入
+                                <span className="text-[#538aa2] text-[1.042vw] font-normal font-Baloo_Thambi leading-[1.257vw] tracking-[0.156vw]">Naturs
+                                    <span className="text-[#399983] text-[1.042vw] font-normal font-Baloo_Thambi leading-[1.257vw] tracking-[0.156vw]">ai
+                                        <span className="text-[#3F3F46] text-[1.042vw] font-normal font-Noto_Sans_ch leading-[1.257vw] tracking-[0.156vw]">所有產品，體驗真實的生活管理系統。<br /></span>
                                     </span>
                                 </span>
                             </span>
 
-                            <button className='mt-[0.768vh] w-[11.667vw] h-[6.147vh] bg-gradient-to-r from-[#399983] to-[#538aa2] flex justify-center items-center hover:from-[#3F3F46] hover:to-[#3F3F46]'>
-                                <Link to='/product' className='text-center text-white text-[2.634vh] font-medium font-Noto_Sans_ch leading-[1.508vw] '>深入了解</Link>
+                            <button className='mt-[0.365vw] w-[11.667vw] h-[2.917vw] bg-gradient-to-r from-[#399983] to-[#538aa2] flex justify-center items-center hover:from-[#3F3F46] hover:to-[#3F3F46]'>
+                                <Link to='/product' className='text-center text-white text-[1.250vw] font-medium font-Noto_Sans_ch leading-[1.508vw] '>深入了解</Link>
                             </button>
                         </div>
-                        <div className='mr-[0.8vw] mt-auto w-[11.667vw] h-[37.980vh] flex flex-col justify-between'>
-                            <button className='w-full h-[6.147vh] bg-gradient-to-r from-[#399983] to-[#538aa2] flex justify-center items-center hover:from-[#3F3F46] hover:to-[#3F3F46]'>
-                                <Link to='/Information' className='text-center text-white text-[2.634vh] font-medium font-Noto_Sans_ch leading-[1.508vw]'>新聞動態</Link>
+                        <div className='mr-[0.8vw] mt-auto w-[11.667vw] h-[18.021vw] flex flex-col justify-between'>
+                            <button className='w-full h-[2.917vw] bg-gradient-to-r from-[#399983] to-[#538aa2] flex justify-center items-center hover:from-[#3F3F46] hover:to-[#3F3F46]'>
+                                <Link to='/Information' className='text-center text-white text-[1.250vw] font-medium font-Noto_Sans_ch leading-[1.508vw]'>新聞動態</Link>
                             </button>
-                            <button className='w-full h-[6.147vh] bg-gradient-to-r from-[#399983] to-[#538aa2] flex justify-center items-center hover:from-[#3F3F46] hover:to-[#3F3F46]'>
-                                <Link to='/product' className='text-center text-white text-[2.634vh] font-medium font-Noto_Sans_ch leading-[1.508vw]'>健康管理APP</Link>
+                            <button className='w-full h-[2.917vw] bg-gradient-to-r from-[#399983] to-[#538aa2] flex justify-center items-center hover:from-[#3F3F46] hover:to-[#3F3F46]'>
+                                <Link to='/product' className='text-center text-white text-[1.250vw] font-medium font-Noto_Sans_ch leading-[1.508vw]'>健康管理APP</Link>
                             </button>
                         </div>
                     </div>
-                    <div className='flex w-[88.646vw] h-[34.907vh] ml-[4.375vw]'>
-                        <div className='w-[29.427vw] h-[30.406vh]  flex flex-col  space-y-[4.1vh]'>
-                            <span className='w-[21.354vw] h-[5.488vh] text-white text-[1.667vw] font-normal font-Baloo_Thambi leading-[6.323vh] tracking-[0.527vh]'>健康管理首選</span>
-                            <span className='w-[w-[29.427vw] h-[11.526vh]  text-white text-[1.042vw] font-normal font-Noto_Sans_ch leading-[3.842vh] tracking-[0.156vw]'>線上的健康管理師，您將於日常紀錄餐點並透過專業醫療人員進一步引導與協助，管理的瑣碎由Natursai為您把關，提供優質個人化健康管理，讓管理成為日常。</span>
+                    <div className='flex w-[88.646vw] h-[16.562vw] ml-[4.375vw]'>
+                        <div className='w-[29.427vw] h-[14.427vw]  flex flex-col  space-y-[1.945vw]'>
+                            <span className='w-[21.354vw] h-[2.604vw] text-white text-[1.667vw] font-normal font-Baloo_Thambi leading-[3.000vw] tracking-[0.250vw]'>健康管理首選</span>
+                            <span className='w-[w-[29.427vw] h-[5.469vw]  text-white text-[1.042vw] font-normal font-Noto_Sans_ch leading-[1.823vw] tracking-[0.156vw]'>線上的健康管理師，您將於日常紀錄餐點並透過專業醫療人員進一步引導與協助，管理的瑣碎由Natursai為您把關，提供優質個人化健康管理，讓管理成為日常。</span>
                         </div>
-                        <div className='flex w-[43.385vw] h-[9.660vh] mt-[21.3vh] ml-[15.4vw] justify-between'>
-                            <div className='flex w-[18.438vw]  h-[9.660vh] justify-between items-end '>
+
+                        <div className='flex w-[43.385vw] h-[4.583vw] mt-[10.106vw] ml-[15.4vw] justify-between'>
+                            <div className='flex w-[18.438vw]  h-[4.583vw] justify-between items-end '>
                                 <img className='w-[4.583vw]' src={NatursAi} />
                                 <div className='flex flex-col justify-between w-[12.760vw]  h-full ml-[1.144vw]'>
-                                    <div className='w-[8.281vw] h-[4.830vh] ml-[0.260vw] flex'>
-                                        <span className="mt-[1vh] text-[#3F3F46] font-Baloo_Thambi font-normal text-[1.042vw] leading-[2.649vh] tracking-[0.156vw] ">Natursai&nbsp;</span>
-                                        <span className='mt-[1vh] text-[#3F3F46] text-[1.042vw] font-Noto_Sans_ch font-bold leading-[2.481vh]  tracking-[0.156vw] text-center'>團隊</span>
+                                    <div className='w-[8.281vw] h-[2.217vw] ml-[0.260vw] flex'>
+                                        <span className="mt-[0.469vw] text-[#3F3F46] font-Baloo_Thambi font-normal text-[1.042vw] leading-[1.257vw] tracking-[0.156vw] ">Natursai&nbsp;</span>
+                                        <span className='mt-[0.469vw] text-[#3F3F46] text-[1.042vw] font-Noto_Sans_ch font-bold leading-[1.177vw]  tracking-[0.156vw] text-center'>團隊</span>
                                     </div>
 
-                                    <Link to='/about' className='w-[12.760vw] h-[4.987vh]'><HoverImage hoverSrc={Contact_us_green} defaultSrc={Contact_us} /></Link>
+                                    <Link to='/about' className='w-[12.760vw] h-[2.395vw]'><HoverImage hoverSrc={Contact_us_green} defaultSrc={Contact_us} /></Link>
                                 </div>
                             </div>
-                            <div className='flex w-[18.438vw]  h-[9.660vh] justify-between items-end '>
+                            <div className='flex w-[18.438vw]  h-[4.583vw] justify-between items-end '>
                                 <img className='w-[4.583vw]' src={GoEat} />
                                 <div className='flex flex-col justify-between w-[12.760vw]  h-full ml-[1.144vw]'>
-                                    <div className="w-[8.281vw] h-[4.830vh]  flex ">
-                                        <span className='mt-[1vh] text-[#3F3F46] text-[1.042vw] font-Noto_Sans_ch font-bold leading-[2.481vh] tracking-[0.156vw]'>夠益營養APP</span>
+                                    <div className="w-[8.281vw] h-[2.217vw]  flex ">
+                                        <span className='mt-[0.469vw] text-[#3F3F46] text-[1.042vw] font-Noto_Sans_ch font-bold leading-[1.177vw] tracking-[0.156vw]'>夠益營養APP</span>
                                     </div>
-                                    <Link to='/product' className='w-[12.760vw] h-[4.987vh]'><HoverImage hoverSrc={deep_understand_green} defaultSrc={deep_understand} /></Link>
+                                    <Link to='/product' className='w-[12.760vw] h-[2.395vw]'><HoverImage hoverSrc={deep_understand_green} defaultSrc={deep_understand} /></Link>
                                 </div>
                             </div>
                         </div>
+
                     </div>
-                    <div className="ml-[7.187vw] mt-[36.5vh] w-[31.146vw] h-[43.249vh] flex flex-col">
-                        <div className='w-[27.135vw] h-[12.953vh] flex flex-col' >
-                            <span className="text-[#399983] text-[1.667vw] font-bold font-Noto_Sans_ch leading-[3.952vh] tracking-[0.250vw]">專業首選</span>
-                            <span className="text-[#538aa2] text-[1.667vw] font-bold font-Noto_Sans_ch leading-[3.952vh] tracking-[0.250vw]">的健康管理團隊</span>
+
+                    <div className="ml-[7.187vw] mt-[17.318vw] w-[31.146vw] h-[20.520vw] flex flex-col">
+                        <div className='w-[27.135vw] h-[6.146vw] flex flex-col' >
+                            <span className="text-[#399983] text-[1.667vw] font-bold font-Noto_Sans_ch leading-[1.850vw] tracking-[0.250vw]">專業首選</span>
+                            <span className="text-[#538aa2] text-[1.667vw] font-bold font-Noto_Sans_ch leading-[1.850vw] tracking-[0.250vw]">的健康管理團隊</span>
                         </div>
-                        <span className='w-[30.208vw] h-[8.782vh] text-[#3F3F46] text-[1.042vw] font-normal font-Noto_Sans_ch leading-[2.649vh] tracking-[0.156vw]'>立即訂閱
-                            <span className='text-[#538AA2] text-[1.042vw] font-Baloo_Thambi font-normal leading-[2.649vh] tracking-[0.156vw]'>Naturs
-                                <span className='text-[#399983] text-[1.042vw] font-Baloo_Thambi font-normal leading-[2.649vh] tracking-[0.156vw]'>ai
-                                    <span className='text-[#3F3F46] text-[1.042vw] font-Noto_Sans_ch font-normal leading-[2.649vh] tracking-[0.156vw] '>電子報，以了解我們所有產品和公告的最新資訊。
+
+                        <span className='w-[30.208vw] h-[4.167vw] text-[#3F3F46] text-[1.042vw] font-normal font-Noto_Sans_ch leading-[1.257vw] tracking-[0.156vw]'>立即訂閱
+                            <span className='text-[#538AA2] text-[1.042vw] font-Baloo_Thambi font-normal leading-[1.257vw] tracking-[0.156vw]'>Naturs
+                                <span className='text-[#399983] text-[1.042vw] font-Baloo_Thambi font-normal leading-[1.257vw] tracking-[0.156vw]'>ai
+                                    <span className='text-[#3F3F46] text-[1.042vw] font-Noto_Sans_ch font-normal leading-[1.257vw] tracking-[0.156vw] '>電子報，以了解我們所有產品和公告的最新資訊。
                                     </span>
                                 </span>
                             </span>
                         </span>
-                        <div className='w-[31.146vw] h-[3.952vh] flex justify-between'>
-                            <input className='w-[21.042vw] h-[3.952vh] border-[0.104vw] border-[#b3b3b3] bg-transparent rounded-[0.417vw] pl-[0.625vw] text-[0.625vw]' />
-                            <button className='w-[7.187vw] h-[3.732vh] bg-gradient-to-r from-[#399983] to-[#538aa2] flex items-center justify-center text-white text-[0.833vw] hover:from-[#3F3F46] hover:to-[#3F3F46]'>訂閱</button>
+
+                        <div className='w-[31.146vw] h-[1.875vw] flex justify-between'>
+                            <input className='w-[21.042vw] h-[1.875vw] border-[0.104vw] border-[#b3b3b3] bg-transparent rounded-[0.417vw] pl-[0.625vw] text-[0.625vw]' />
+                            <button className='w-[7.187vw] h-[1.875vw] bg-gradient-to-r from-[#399983] to-[#538aa2] flex items-center justify-center text-white text-[0.833vw] hover:from-[#3F3F46] hover:to-[#3F3F46]'>訂閱</button>
                         </div>
-                        <div className='mt-[1.976vh] w-[22.604vw] h-[3.293vh] flex items-center'>
+
+                        <div className='mt-[0.938vw] w-[22.604vw] h-[1.562vw] flex items-center'>
                             <input
                                 type="checkbox"
-                                className={`w-[0.938vw] h-[1.976vh] rounded-md ${isChecked ? 'accent-[#538AA2]' : 'accent-[#538AA2]'
+                                className={`w-[0.938vw] h-[0.938vw] rounded-md ${isChecked ? 'accent-[#538AA2]' : 'accent-[#538AA2]'
                                     }`}
                                 checked={isChecked}
                                 onChange={handleCheckboxChange}
                             />
-                            <span className=' ml-[0.312vw] text-[0.573vw] text-[#3F3F46] font-Noto_Sans_ch font-normal leading-[1.457vh] tracking-[0.086vw]'>我同意接收來自 NatursAi 的信息分享。</span>
+                            <span className=' ml-[0.312vw] text-[0.573vw] text-[#3F3F46] font-Noto_Sans_ch font-normal leading-[0.691vw] tracking-[0.086vw]'>我同意接收來自 NatursAi 的信息分享。</span>
                         </div>
-                        <span className='mt-[1vh] w-[21.042vw] h-[10.209vh] text-[#3F3F46] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[1.457vh] tracking-[0.086vw]'>您可以隨時取消訂閱這些通訊。欲了解如何取消訂閱、我們的隱私實踐以及我們如何承諾保護和尊重您的隱私，請參閱我們的
-                            <span className='text-[#538AA2] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[1.457vh] tracking-[0.086vw]'>《
-                                <span className='text-[#538AA2] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[1.457vh] tracking-[0.086vw]'><span className='underline decoration-[0.052vw]'>隱私政策</span>
-                                    <span className='text-[#538AA2] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[1.457vh] tracking-[0.086vw]'>》
-                                        <span className='text-[#3F3F46] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[1.457vh] tracking-[0.086vw]'><br/><br/>點擊下方的「訂閱」按鈕，即表示您同意允許 <span className=' font-Noto_Sans_en'>NatursAi</span> 存儲並處理您提交的個人信息，以便為您提供所需的內容。
+                        <span className='mt-[0.474vw] w-[21.042vw] h-[4.844vw] text-[#3F3F46] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[0.691vw] tracking-[0.086vw]'>您可以隨時取消訂閱這些通訊。欲了解如何取消訂閱、我們的隱私實踐以及我們如何承諾保護和尊重您的隱私，請參閱我們的
+                            <span className='text-[#538AA2] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[0.691vw] tracking-[0.086vw]'>《
+                                <span className='text-[#538AA2] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[0.691vw] tracking-[0.086vw]'><span className='underline decoration-[0.052vw]'>隱私政策</span>
+                                    <span className='text-[#538AA2] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[0.691vw] tracking-[0.086vw]'>》
+                                        <span className='text-[#3F3F46] text-[0.573vw] font-Noto_Sans_ch font-normal leading-[0.691vw] tracking-[0.086vw]'><br/><br/>點擊下方的「訂閱」按鈕，即表示您同意允許 <span className=' font-Noto_Sans_en'>NatursAi</span> 存儲並處理您提交的個人信息，以便為您提供所需的內容。
                                         </span>
                                     </span>
                                 </span>
